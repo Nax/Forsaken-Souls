83a58019ba35961b92b336f246b88aa8514f24fc
Florian FOURATIER <ffourati@e2r12p17.42.fr>
2015-05-13 15:56:10 +0200

    Commented the map export script.

--- utils/build_level.rb
+++ utils/build_level.rb
@@ -17,20 +17,32 @@ class Map
 
   def parse_map m
     map_json = JSON.parse(File.read m)
+
+	# Instantiate map header.
     w = map_json['width']
     h = map_json['height']
+
+	# Instantiate the 5 tile layers.
     data = map_json['layers'].map{|d| d['data'].each_slice(w).to_a.reverse if d['data']}.flatten.select{|i| !i.nil?}
+
+	# Instantiate the object layers.
     objects = map_json['layers'][5]['objects']
+
+	# -- Gather and instantiate spawners.
     spawns = []
     objects.each do |o|
       next if o['type'] != 'spawn'
       spawns << {x: o['x'].to_i / 128, y: h - o['y'].to_i / 128, id: o['properties']['enemyID'].to_i}
     end
+
+	# -- Gather and instantiate lights.
     lights = []
     objects.each do |o|
       next if o['type'] != 'light'
       lights << {x: o['x'].to_f / 128.0, y: h.to_f - o['y'].to_f / 128.0}
     end
+
+	# Pack components into binary data.
     header = [w, h].pack 'LL'
     bindata = data.pack 'S*'
     spawndata = [spawns.length].pack 'L'
